da2f9c0406a78acbe716caf4dff984011dc76a4f
henry.wang <hwang2@appen.com>
2022-06-02 18:40:37 +0800

    update

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "peutils",
-    version = '0.0.16',
+    version = '0.0.17',
     keywords = ("pip3", "peutils",'henry'),
     description = "utils for text,audio,video,excel,file and so on.",
     long_description = "utils for text,audio,video,excel,file and so on,more details please visit gitlab.",
